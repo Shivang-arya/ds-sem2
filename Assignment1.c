2121e12b595584b384596c1ecfb3ce35fd7fb337
Shivang-arya <70498453+Shivang-arya@users.noreply.github.com>
2020-08-31 09:15:15 +0530

    Update and rename struct.c to Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -3,7 +3,7 @@ struct stru
 {
 	char str[20];
 	int number;
-	float fltnumber;
+	float floatnumber;
 };
 
 void putstruct1(struct stru s1)
@@ -11,36 +11,35 @@ void putstruct1(struct stru s1)
 
 	printf("\nChar array : %s ",s1.str);
 	printf("\nInt  : %d",s1.number);
-	printf("\nFloat : %f",s1.fltnumber);
+	printf("\nFloat : %f",s1.floatnumber);
 }
 
 void putstruct2(struct stru *s1)
 {
 	printf("\nChar array : %s ",s1->str);
 	printf("\nInt  : %d",s1->number);
-	printf("\nFloat : %f\n",s1->fltnumber);
+	printf("\nFloat : %f\n",s1->floatnumber);
 }
 
 int main()
 {
 	struct stru str1;
-	printf("---Pass by value---\n");
+	printf("Pass by value\n");
 	printf("Enter a string-");
 	scanf("%s",str1.str);
 	printf("Enter an Integer-");
 	scanf("%d",&str1.number);
 	printf("Enter a decimal-");
-	scanf("%f",&str1.fltnumber);
+	scanf("%f",&str1.floatnumber);
 
 	putstruct1(str1);
-	printf("\n----------------------------------------------------\n");
-	printf("---Pass by reference---\n");
+	printf("Pass by reference\n");
 	printf("Enter a string-");
 	scanf("%s",str1.str);
 	printf("Enter an Integer-");
 	scanf("%d",&str1.number);
 	printf("Enter a decimal-");
-	scanf("%f",&str1.fltnumber);
+	scanf("%f",&str1.floatnumber);
 
 	putstruct2(&str1);
 
